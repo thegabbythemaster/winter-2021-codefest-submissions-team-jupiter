d7dca33e5d71f0dd7fc13ed683e4588ccdc44fc4
Gabriella Alexis <Gabby@Gabbys-Mac.home>
2021-01-11 23:17:47 -0500

    user login and signup forms

--- js/script.js
+++ js/script.js
@@ -16,3 +16,29 @@ function loadDoc() {
     xhttp.open("GET", "sample.txt", true);
     xhttp.send();
 }
+
+function openLoginForm() {
+    document.body.classList.add("showLoginForm");
+}
+function closeLoginForm() {
+    document.body.classList.remove("showLoginForm");
+    console.log("close modal with x button");
+}
+
+function openSignupForm() {
+    document.body.classList.add("showSignupForm");
+}
+function closeSignupForm() {
+    document.body.classList.remove("showSignupForm");
+}
+
+// Get the modal
+var modal = document.getElementById("login");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        document.body.classList.remove("showLoginForm");
+        console.log("close modal");
+    }
+}
