9cf6d4f8b2b00c4bfd255aabadb965c3ea7ec6c8
Gabriella Alexis <Gabby@Gabbys-Mac.local>
2021-01-24 14:12:20 -0500

    make suggestion feature

--- js/script.js
+++ js/script.js
@@ -19,6 +19,59 @@ function loadDoc() {
     xhttp.open("GET", "sample.txt", true);
     xhttp.send();
 }
- 
+
 //-------------------------------------------------------------------------------------------
 
+// jQuery to redirect home page to other pages
+$(document).ready(function () {
+    $("#pick-suggestion").click(function () {
+        window.location.href = "PickSuggestion.html";
+    });
+
+    $("#make-suggestion").click(function () {
+        window.location.href = "make-suggestion.html";
+    });
+
+    $("#mental-health").click(function () {
+        window.location.href = "categories/mental-health.html";
+    });
+    $("#diy").click(function () {
+        window.location.href = "categories/diy.html";
+    });
+    $("#how-to").click(function () {
+        window.location.href = "categories/how-to.html";
+    });
+    $("#physical-activities").click(function () {
+        window.location.href = "categories/physical-activities.html";
+    });
+    $("#other").click(function () {
+        window.location.href = "categories/other.html";
+    });
+});
+
+//Login/Signup form popup control
+function openLoginForm() {
+    document.body.classList.add("showLoginForm");
+}
+function closeLoginForm() {
+    document.body.classList.remove("showLoginForm");
+    console.log("close modal with x button");
+}
+
+function openSignupForm() {
+    document.body.classList.add("showSignupForm");
+}
+function closeSignupForm() {
+    document.body.classList.remove("showSignupForm");
+}
+
+// Get the modal
+var modal = document.getElementById("login");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        document.body.classList.remove("showLoginForm");
+        console.log("close modal");
+    }
+}
